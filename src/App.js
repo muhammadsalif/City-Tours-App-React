abbfcd4a0292ba298040b82b259545125a3d11e3
Muhammad Salif <muhammad.salif@yahoo.com>
2020-05-04 00:20:45 +0500

    Navbar HTML Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
 import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World.</h1>
-      </div>
+      <React.Fragment>
+        <Navbar></Navbar>
+        <div>
+          <h1>Hello World.</h1>
+        </div>
+      </React.Fragment>
     );
   }
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,26 @@
 import React from "react";
-
+import logo from "../../logo.svg";
 export default function Navbar() {
-  return <div>Hello From Navbar.</div>;
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="City Tours App." />
+      <ul className="nav-links">
+        <li>
+          <a href="/" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link active">
+            tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
