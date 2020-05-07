ae46fdef87b6e8dd6387fa3d425f6021d5acb8b6
Muhammad Salif <muhammad.salif@yahoo.com>
2020-05-07 07:39:16 +0500

    Toggle info completed

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -1,6 +1,14 @@
 import "./tour.scss";
 import React, { Component } from "react";
 class Tour extends Component {
+  state = {
+    tourInfo: false,
+  };
+
+  handleInfo = () => {
+    this.setState({ tourInfo: !this.state.tourInfo });
+  };
+
   render() {
     const { id, name, city, img, info } = this.props.tour;
     const { removeTour } = this.props;
@@ -18,11 +26,11 @@ class Tour extends Component {
           <h4>{name}</h4>
           <h5>
             info{" "}
-            <span>
+            <span onClick={this.handleInfo}>
               <i className="fas fa-caret-square-down"></i>
             </span>
           </h5>
-          <p>{info}</p>
+          {this.state.tourInfo && <p>{info}</p>}
         </div>
       </article>
     );
